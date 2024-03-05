94c04623e2dcecbe954b396727cfd2142058b360
duweri <danielleuweri01@gmail.com>
2024-03-05 14:27:56 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+// import Apollo server
+const { ApolloServer } = require("apollo-server-express");
+// middleware function
+const {authMiddleware} = require ("./utils/auth");
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
